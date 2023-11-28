d3ce3386c045a2aa0e45b19d87cad1b5a3305708
alizaidmerchant <151141339+alizaidmerchant@users.noreply.github.com>
2023-11-28 12:10:37 +0530

    Add files via upload

---
+++ airnb.py
@@ -0,0 +1,17 @@
+import requests
+import pandas as pd 
+from bs4 import BeautifulSoup      
+
+url = "https://www.pw.live/online-course-physics-wallah-gate-ds-and-ai/parakaram-gate-2024-data-science-ai"
+r = requests.get(url)
+# print(r)
+
+soup = BeautifulSoup(r.text,"lxml")
+details = soup.find("div",class_= "batch font-fam-medium")
+# print(details.text)
+
+teachers = soup.find_all("span",class_="font-fam-bold")
+print(teachers)
+# # print(len(names))
+# for name in names:
+#     print(name)
