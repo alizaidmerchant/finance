8ab7784b170ad50861d58a1d16c7b5e0baf7844e
alizaidmerchant <151141339+alizaidmerchant@users.noreply.github.com>
2023-11-28 12:35:37 +0530

    Add files via upload

---
+++ amazon.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+url = "https://in.indeed.com/"
+r = requests.get(url)
+print(r)
+# soup = BeautifulSoup(r.text,"lxml")
+
+# names = soup.find("div",{"class":"_4rR01T"})
+# print(names)
+
+# for name in names:
+     
+#     print(name.text)
+
+# prices = soup.find("div",{""})
+# print(prices)
+
+# for price in prices:
+
+#     print(price.text)
